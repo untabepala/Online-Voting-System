503e26a8f8a37f649595be4ee568964012e0e977
Nayani Tharushika <118815958+untabepala@users.noreply.github.com>
2023-11-21 00:42:14 +0530

    Add files via upload

---
+++ routes/dashboard.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Online Voting System - Dashboard</title>
+    </head>
+    <body>
+        <button>Back</button>
+        <button>Logout</button>
+        <h1>Online Voting System</h1>
+        <hr>
+        <div id="profile"></div>
+        <div id="Group"></div>
+    </body>
+</html>
+    
